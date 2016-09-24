ecf65ff721f5488b5b041902d6a1fe8803184251
Billingham <laurence@bgs.ac.uk>
2016-09-24 17:26:33 +0100

    added a drafts list view

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     url(r'^post/(?P<pk>\d+)$', views.post_detail, name='post_detail'),
     url(r'^post/(?P<pk>\d+)/edit$', views.post_edit, name='post_edit'),
     url(r'^$', views.post_list, name='post_list'),
+    url(r'^drafts$', views.post_draft_list, name='post_draft_list'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.shortcuts import get_object_or_404, redirect, render
 from django.utils import timezone
 
@@ -8,6 +9,13 @@ def post_list(request):
     posts = Post.objects.filter(
         published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_draft_list(request):
+    posts = Post.objects.filter(
+        Q(published_date__gt=timezone.now()) | Q(published_date__isnull=True)
+    ).order_by('created_date')
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
